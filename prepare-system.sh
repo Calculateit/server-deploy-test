1dc0a0e5ebd55448d03a63a68821233ced427f41
Dmitrii Daragan <dm.daragan1@yandex.ru>
2023-05-28 20:48:50 +0300

    rename & fix cmd symbols bug

--- prepare-system.sh
+++ prepare-system.sh
@@ -20,21 +20,14 @@ sudo docker network create --driver bridge reverse-proxy
 
 # Установка nginx-proxy и настройка на проброс 80 (HTTP) и 443 (HTTPS) портов
 sudo docker run -d -p 80:80 -p 443:443 \
- --name nginx-proxy \
- --network reverse-proxy \
- --restart=always \
- -v $HOME/certs:/etc/nginx/certs:ro \
- -v /etc/nginx/vhost.d \
- -v /usr/share/nginx/html \
+ --name nginx-proxy     --network reverse-proxy \
+ --restart=always     -v $HOME/certs:/etc/nginx/certs:ro \
+ -v /etc/nginx/vhost.d     -v /usr/share/nginx/html     \
  -v /var/run/docker.sock:/tmp/docker.sock:ro \
- -label com.github.jrcs.letsencrypt_nginx_proxy_companion.nginx_proxy=true jwilder/nginx-proxy
-
-sudo docker run -d --name nginx-letsencrypt \
- --restart=always \
- --volumes-from nginx-proxy \
- --network reverse-proxy \
- -v $HOME/certs:/etc/nginx/certs:rw \
- -v /var/run/docker.sock:/var/run/docker.sock:ro jrcs/letsencrypt-nginx-proxy-companion
+     --label com.github.jrcs.letsencrypt_nginx_proxy_companion.nginx_proxy=true \
+jwilder/nginx-proxy
+sudo docker run -d     --name nginx-letsencrypt     --restart=always     --volumes-from nginx-proxy     --network reverse-proxy \
+-v $HOME/certs:/etc/nginx/certs:rw   -v /var/run/docker.sock:/var/run/docker.sock:ro     jrcs/letsencrypt-nginx-proxy-companion
 
 sudo mkdir /var/projects
 sudo groupadd projects
