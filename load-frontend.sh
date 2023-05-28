744d8985b1cd9bcea65afd408ee31fe8b8c53794
Dmitrii Daragan <dm.daragan1@yandex.ru>
2023-05-28 21:28:14 +0300

    refaactor

--- load-frontend.sh
+++ load-frontend.sh
@@ -15,19 +15,12 @@ echo "Working branch " $branch
 # curl -F chat_id=$TELEGRAM_ADMIN_CHAT -F text="start deploy ${BASE_DOMAIN} ${branch}" \
 # https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage
 
-folders=( \
-    $FRONTEND_SRC_DIR \
-      )
-
-for folder in ${folders[*]}
-do
-    echo "update " $folder
-    cd $folder && git clone git@github.com:hhru-school/techradar.git
-    git checkout $branch
-    echo "building frontend "
-    cd $folder && npm i
-    cd $folder && npm build react-app-rewired -y
-done
+echo "update " $FRONTEND_SRC_DIR
+cd $FRONTEND_SRC_DIR && git clone git@github.com:hhru-school/techradar.git
+git checkout $FRONTEND_SRC_DIR
+echo "building frontend "
+cd $FRONTEND_SRC_DIR && npm i
+cd $FRONTEND_SRC_DIR && npm build react-app-rewired -y
 
 cd $prodDir && docker-compose up -d --build
 
